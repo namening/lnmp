69050a0c374fb18687969d91e4138f60258dedff
namening <41912290+namening@users.noreply.github.com>
2018-08-06 16:49:53 +0800

    Add files via upload

--- 安装nginx.sh
+++ 安装nginx.sh
@@ -284,4 +284,115 @@ done
 		access_log /tmp/1.log combined_realip;
 	}
 	使用location~可以指定对应的静态文件，expires配置过期时间，而access_log配置为off就可以不记录访问日志了。
+6、Nginx防盗链
+	思路和httpd一样，由于和过期时间、不记录日志有部分重合，可以把2部分组合在一起：
+	location ~* ^.+\.(gif|jpg|png|swf|flv|rar|zip|doc|pdf|gz|bz2|jpeg|bmp|xls)$
+	{
+		expires 7d;
+		valid_referers none blocked server_names *.test.com;
+		if ($invalid_referer) {
+			return 403;
+		}
+		access_log off;
+	}
+7、访问控制
+	和httpd一样，Nginx也需要限制某些IP不能访问或者只允许某些IP访问。配置方法和httpd很像，而且不像httpd那样全部遍历一遍。比如，我们有个需求“使访问admin目录的请求只允许192.168.188.1和127.0.0.1访问”，配置文件如下：
+	location /admin/
+	{
+		allow 192.168.188.1;
+		allow 127.0.0.1;
+		deny all;
+	}
+	在配置httpd，还有一个order，来定义先allow还是先deny，在Nginx里并没有，只要匹配规则就结束了。
+	配置文件中的ip也可以为ip段，比如写成allow192.168.0/24。如果至拒绝某几个ip，就写成这样：
+	location /admin/
+	{
+		deny 192.168.188.1;
+		deny 127.0.0.1;
+	}
+	如果是黑名单的形式，就不要写allow all了，因为默认就是允许所有。也可以根据正则匹配来限制，如下：
+	location ~ .*(abc|image)/.*\.php$
+	{
+		deny all;
+	}
+	小括号里面的竖线为分隔符，它们直接是“或者”的意思，这样就可以把访问的URL中带有abc或者image字符串，并且是PHP的请求拒绝访问。在nginx配置中，就可以把上传文件的目录禁止解析PHP.
+	在nginx配置里，也可以针对user_agent做一些限制，配置如下：
+	if ($http_user_agent ~ 'Spider/3.0|YoudaoBot|Tomato')
+	{
+		return 403;
+	}
+	其中~为匹配符号，只要user_agent中含有Spider/3.0或者YoudaoBot或者Tomato字符串的，都会被拒绝，return403为直接返回403的状态码，当然也可以把它替换为deny all。
+8、nginx解析PHP
+	下面是test.com.conf的内容，其中包含了PHP相关的配置：
+	server
+	{
+		listen 80;
+		server_name test.com test1.com test2.com;
+		index index.html index.htm index.php;
+		root /data/nginx/test.com;
+		
+		if ($host != 'test.com'){
+			rewrite ^/(.*)$ http://test.com/$1 permanent;
+		}
+		
+		location ~ \.php$
+		{
+			include fastcgi_params;
+			fastcgi_pass unix:/tmp/php-fcgi.sock;
+			fastcgi_index index.php;
+			fastcgi_param SCRIPT_FILENAME /data/nginx/test.com$fastcgi_script_name;
+		}
+		access_log /tmp/1.log combined_realip;
+	}
+	其中fastcgi_pass用来指定php-fpm的地址，如果php-fpm监听的是一个tcp:port的地址(比如127.0.0.1:9000),那么也需要在这里改成fastcgi_pass 127.0.0.1:9000。这个地址一定要和php-fpm服务监听的地址匹配，否则会报502错误。
+	还有一个地方需要注意，fastcgi_param SCRIPT_FILENAME后面跟的路径为该站点的根目录，和前面定义的root那个路径保持一致。如果这里配置不对，访问PHP页面会出现404。
+9、Nginx代理
+	如果没有公网IP的服务器需要提供web服务，就可以通过代理来实现。
+	如果nginx后面有多台web服务器，如果同时代理，那nginx在这里就起到一个负载均衡的作用，这个功能在生产环境中用得也比较多。nginx代理配置：
+	#	cd /usr/local/nginx/conf/vhost
+	#	vim proxy.conf
+server
+{
+    listen 80;
+    server_name ask.apelearn.com;
+
+    location /
+    {
+        proxy_pass      http://121.201.9.155/;
+        proxy_set_header Host   $host;
+        proxy_set_header X-Real-IP      $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+    }
+}
+	Proxy_pass指定要代理的域名所在的服务器IP。后面的三行为定义发往后端web服务器的请求头，第二行必须有，否则代理不成功，它表示后端web服务器的域名和当前配置文件中的server_name保持一致（即ask.apelearn.com),第三行和第四行可以忽略，前面在讲述Nginx日志格式的时候介绍过这2个参数。
+	
+	有2个ip就可以走负载均衡了，配置过程如下：
+	#	vim /usr/local/nginx/conf/vhost/load.conf	//写入如下内容：
+upstream qq_com
+{
+    ip_hash;
+    server 61.135.157.156:80;
+    server 125.39.240.113:80;
+}
+
+server
+{
+    listen 80;
+    server_name www.qq.com;
+
+    location /
+    {
+        proxy_pass      http://qq_com;
+        proxy_set_header Host   $host;
+        proxy_set_header X-Real-IP      $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+    }
+}
+	和简单的代理不同，负载均衡多了一个upstream，在这里定义后端的web server，可以是一个，也可以是多个。其中ip_hash为负载均衡的算法，它表示根据ip地址把请求分到不同的服务器上。
+10、nginx配置ssl
+	#	cd /usr/local/nginx/conf
+	#
+
+
+
 
